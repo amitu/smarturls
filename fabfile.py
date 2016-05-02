1cfea9ee07f4b37288dc799e071e997b62ef4d0d
Amit Upadhyay <code@amitu.com>
2016-05-02 17:22:03 +0530

    minor

--- fabfile.py
+++ fabfile.py
@@ -3,9 +3,9 @@ from fabric.api import local, cd
 
 def docs():
     local("./bin/docs")
-    local("./bin/python3 setup.py upload_sphinx --upload-dir=docs/html")
+    local("./bin/python setup.py upload_sphinx --upload-dir=docs/html")
 
 
 def release():
     # update version id in setup.py, changelog and docs/source/conf.py
-    local("python3 setup.py bdist_egg sdist --formats=bztar,gztar,zip upload")
+    local("python setup.py bdist_wheel sdist --formats=bztar,zip upload")
