96e327bda77c8f2e1a856a5a630a9c6c7a5f602a
juancarlospaco <juancarlospaco@gmail.com>
2017-06-03 02:22:00 -0300

    Update setup.py bump versions, add metadata.

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
 
 try:
@@ -5,16 +7,17 @@ try:
 except IOError:
     long_description = ""
 
+
 setup(
     name="smarturls",
-    description="a url construction helper for django",
+    description="A URL construction helper for Django.",
     long_description=long_description,
 
-    version="0.1.7",
+    version="0.1.8",
     author='Amit Upadhyay',
     author_email="upadhyay@gmail.com",
 
-    url='http://amitu.com/smarturls/',
+    url='https://amitu.com/smarturls/',
     license='BSD',
 
     py_modules=["smarturls"],
@@ -22,4 +25,28 @@ setup(
     zip_safe=True,
 
     install_requires=["Django>=1.5"],
+
+    tests_require=['tox'],
+
+    keywords=['helper', 'django', 'utils', 'regex', 'utility'],
+
+    classifiers=[
+
+        'Intended Audience :: Developers',
+
+        'Natural Language :: English',
+
+        'Operating System :: OS Independent',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+
+        'Programming Language :: Python :: Implementation :: CPython',
+
+        'Topic :: Software Development',
+
+    ],
 )
